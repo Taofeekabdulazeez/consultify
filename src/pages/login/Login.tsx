172b6ea4fadd6722206b57ba2ffe6df626e01ba9
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2024-05-14 22:17:44 +0100

    Refactoring

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,32 +1,38 @@
+import "./login.css";
+
 export default function Login() {
   return (
-    <div className="container">
-      <h1>Welcome</h1>
-      <div className="buttons">
-        <button className="signup">Sign Up</button>
-        <button className="signin">Sign In</button>
-      </div>
-      <form>
-        <input type="email" placeholder="Email Address" />
-        <input type="password" id="password" placeholder="Password" />
-        <span className="toggle-password"><i className='bx bx-hide' ></i></span>
-        <button type="submit">Sign Up</button>
-      </form>
-      <div className="seperator">
-        <span>OR</span>
-      </div>
-      <div className="social-icons">
-        <div className="icon-circle">
-          <i className="bx bxl-google"></i>
+    <div className="page-container">
+      <div className="container">
+        <h1>Welcome</h1>
+        <div className="buttons">
+          <button className="signup">Sign Up</button>
+          <button className="signin">Sign In</button>
         </div>
-        <div className="icon-circle">
-          <i className="bx bxl-facebook"></i>
+        <form>
+          <input type="email" placeholder="Email Address" />
+          <input type="password" id="password" placeholder="Password" />
+          <span className="toggle-password">
+            <i className="bx bx-hide"></i>
+          </span>
+          <button type="submit">Sign Up</button>
+        </form>
+        <div className="seperator">
+          <span>OR</span>
         </div>
-        <div className="icon-circle">
-          <i className="bx bxl-linkedin"></i>
-        </div>
-        <div className="icon-circle">
-          <i className="bx bxl-github"></i>
+        <div className="social-icons">
+          <div className="icon-circle">
+            <i className="bx bxl-google"></i>
+          </div>
+          <div className="icon-circle">
+            <i className="bx bxl-facebook"></i>
+          </div>
+          <div className="icon-circle">
+            <i className="bx bxl-linkedin"></i>
+          </div>
+          <div className="icon-circle">
+            <i className="bx bxl-github"></i>
+          </div>
         </div>
       </div>
     </div>
